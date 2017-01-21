551867d01cfef8352362ac2bc3598949291011b5
kcalderon87 <kcalderon87@gmail.com>
2017-01-21 15:54:58 -0500

    updated port info

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 var app = express();
-var PORT = 8080;
+var PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
